ff7fb005b66ad95c6a6c7d3a1d2c642d1a7638d1
Luke Eades <lukeeadeswork@gmail.com>
2023-08-23 15:18:48 -0700

    added temperature and location data on search

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ let main = document.querySelector('main');
 let loc = document.getElementById("loc"); 
 let button = document.getElementById("enter"); 
 let results = document.getElementById('result'); 
+let locTitle = document.getElementById('location'); 
+let tempCur = document.getElementById('temp'); 
 window.addEventListener('keydown', e =>{
     if(e.code == "Enter"){
         try{fetch(`http://api.weatherapi.com/v1/current.json?key=9ab67380d0b14cb2aa120218232005&q=${loc.value}&aqi=yes`,{mode: 'cors'}).then(function(response){
@@ -14,14 +16,16 @@ window.addEventListener('keydown', e =>{
         .then(function(json){
             if(json !== null){
                 console.log(json); 
-                results.textContent = json.current.feelslike_c; 
-                main.style.backgroundImage = `url(${json.current.condition.icon})`; 
+                // results.textContent = json.current.feelslike_c; 
+                // main.style.backgroundImage = `url(${json.current.condition.icon})`; 
+                locTitle.innerHTML = `<div id = 'city'>${json.location.name}</div><div id = 'country'>${json.location.country}</div>`;  
+                tempCur.children[0].innerHTML = json.current.temp_f;  
             }else{
                 throw new Error('problem parsing json'); 
             }
         })}
         catch(error){
-            // console.log('not a valid location');
+            console.log(error);
         }
     }
 }); 
