30670f5ca8868bfe2bad60533959ea078444ea2d
Luke Eades <lukeeadeswork@gmail.com>
2023-09-03 13:32:15 -0700

    added calls to get forecast and create day elements

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ let locTitle = document.getElementById('location');
 let tempCur = document.getElementById('temp'); 
 let defaultLoc = "San Luis Obispo"; 
 let forecast = document.getElementById('forecast'); 
+const weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const d = new Date(); 
 
-try{fetch(`http://api.weatherapi.com/v1/current.json?key=9ab67380d0b14cb2aa120218232005&q=${defaultLoc}&aqi=yes`,{mode: 'cors'}).then(function(response){
+try{fetch(`http://api.weatherapi.com/v1/forecast.json?key=9ab67380d0b14cb2aa120218232005&q=${defaultLoc}&days=7&aqi=yes`,{mode: 'cors'}).then(function(response){
     if (response.status !== 200){
         throw new Error('not a valid location'); 
-    }else{
+    }else{ 
         return response.json(); 
     }
 })
@@ -20,8 +22,9 @@ try{fetch(`http://api.weatherapi.com/v1/current.json?key=9ab67380d0b14cb2aa12021
         results.style.backgroundImage = `url(${json.current.condition.icon})`; 
         locTitle.innerHTML = `<div id = 'city'>${json.location.name}</div><div id = 'country'>${json.location.country}</div>`;  
         tempCur.children[0].innerHTML = json.current.temp_f + '℉';  
-        for(let i = 0; i < 7; i++){
+        for(let i = 0; i < json.forecast.forecastday.length; i++){
             let day = document.createElement('div'); 
+            day.innerHTML = `<div class = "dayTitle">${weekdays[d.getDay() + i + 1]}</div><div class = "dayTemp">${json.forecast.forecastday[i].day.avgtemp_f}</div>`; 
             day.classList.add('day'); 
             forecast.appendChild(day); 
         }
@@ -34,7 +37,7 @@ catch(error){
 }
 window.addEventListener('keydown', e =>{
     if(e.code == "Enter"){
-        try{fetch(`http://api.weatherapi.com/v1/current.json?key=9ab67380d0b14cb2aa120218232005&q=${loc.value}&aqi=yes`,{mode: 'cors'}).then(function(response){
+        try{fetch(`http://api.weatherapi.com/v1/forecast.json?key=9ab67380d0b14cb2aa120218232005&q=${loc.value}&days=7&aqi=yes`,{mode: 'cors'}).then(function(response){
             if (response.status !== 200){
                 throw new Error('not a valid location'); 
             }else{
@@ -44,10 +47,15 @@ window.addEventListener('keydown', e =>{
         .then(function(json){
             if(json !== null){
                 console.log(json); 
-                // results.textContent = json.current.feelslike_c; 
                 results.style.backgroundImage = `url(${json.current.condition.icon})`; 
                 locTitle.innerHTML = `<div id = 'city'>${json.location.name}</div><div id = 'country'>${json.location.country}</div>`;  
                 tempCur.children[0].innerHTML = json.current.temp_f + '℉';  
+                for(let i = 0; i < json.forecast.forecastday.length; i++){
+                    let day = document.createElement('div'); 
+                    day.innerHTML = `<div class = "dayTitle">${weekdays[d.getDay() + i + 1]}</div><div class = "dayTemp">${json.forecast.forecastday[i].day.avgtemp_f}</div>`; 
+                    day.classList.add('day'); 
+                    forecast.appendChild(day); 
+                }
             }else{
                 throw new Error('problem parsing json'); 
             }
