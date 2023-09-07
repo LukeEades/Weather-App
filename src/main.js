ded60fb9d07e932f710cf1f778bc18c4a9fc49b4
Luke Eades <lukeeadeswork@gmail.com>
2023-09-07 11:07:53 -0700

    fixed some style and added some stats to the bottom

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 let main = document.querySelector('main'); 
-let loc = document.getElementById("loc"); 
-let button = document.getElementById("enter"); 
+let loc = document.getElementById('loc'); 
+let button = document.getElementById('enter'); 
 let results = document.getElementById('result'); 
 let locTitle = document.getElementById('location'); 
 let tempCur = document.getElementById('temp'); 
-let defaultLoc = "San Luis Obispo"; 
+let defaultLoc = 'San Luis Obispo'; 
 let forecast = document.getElementById('forecast'); 
-const weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 const d = new Date(); 
-
+let stats = document.getElementById('stats'); 
 try{fetch(`http://api.weatherapi.com/v1/forecast.json?key=9ab67380d0b14cb2aa120218232005&q=${defaultLoc}&days=7&aqi=yes`,{mode: 'cors'}).then(function(response){
     if (response.status !== 200){
         throw new Error('not a valid location'); 
@@ -24,10 +24,12 @@ try{fetch(`http://api.weatherapi.com/v1/forecast.json?key=9ab67380d0b14cb2aa1202
         tempCur.children[0].innerHTML = json.current.temp_f + '℉';  
         for(let i = 0; i < json.forecast.forecastday.length; i++){
             let day = document.createElement('div'); 
-            day.innerHTML = `<div class = "dayTitle">${weekdays[d.getDay() + i + 1 >= weekdays.length ? (d.getDay() + i + 1) - weekdays.length: d.getDay() + i + 1]}</div><div class = "dayTemp">${json.forecast.forecastday[i].day.avgtemp_f}</div>`; 
+            day.innerHTML = `<div class = 'dayTitle'>${weekdays[d.getDay() + i + 1 >= weekdays.length ? (d.getDay() + i + 1) - weekdays.length: d.getDay() + i + 1]}</div><div class = 'dayTemp'>${json.forecast.forecastday[i].day.avgtemp_f}℉</div>`; 
             day.classList.add('day'); 
             forecast.appendChild(day); 
         }
+        setStats(json); 
+
     }else{
         throw new Error('problem parsing json'); 
     }
@@ -36,7 +38,7 @@ catch(error){
     console.log(error);
 }
 window.addEventListener('keydown', e =>{
-    if(e.code == "Enter"){
+    if(e.code == 'Enter'){
         try{fetch(`http://api.weatherapi.com/v1/forecast.json?key=9ab67380d0b14cb2aa120218232005&q=${loc.value}&days=7&aqi=yes`,{mode: 'cors'}).then(function(response){
             if (response.status !== 200){
                 throw new Error('not a valid location'); 
@@ -50,10 +52,11 @@ window.addEventListener('keydown', e =>{
                 results.style.backgroundImage = `url(${json.current.condition.icon})`; 
                 locTitle.innerHTML = `<div id = 'city'>${json.location.name}</div><div id = 'country'>${json.location.country}</div>`;  
                 tempCur.children[0].innerHTML = json.current.temp_f + '℉';
-                forecast.innerHTML = ""; 
+                forecast.innerHTML = ''; 
+                stats.innerHTML = ''; 
                 for(let i = 0; i < json.forecast.forecastday.length; i++){
                     let day = document.createElement('div'); 
-                    day.innerHTML = `<div class = "dayTitle">${weekdays[d.getDay() + i + 1 >= weekdays.length ? (d.getDay() + i + 1) - weekdays.length: d.getDay() + i + 1]}</div><div class = "dayTemp">${json.forecast.forecastday[i].day.avgtemp_f}</div>`; 
+                    day.innerHTML = `<div class = 'dayTitle'>${weekdays[d.getDay() + i + 1 >= weekdays.length ? (d.getDay() + i + 1) - weekdays.length: d.getDay() + i + 1]}</div><div class = 'dayTemp'>${json.forecast.forecastday[i].day.avgtemp_f}℉</div>`; 
                     day.classList.add('day'); 
                     forecast.appendChild(day); 
                 }
@@ -67,4 +70,10 @@ window.addEventListener('keydown', e =>{
     }
 }); 
 
-
+let setStats = (json)=>{
+    let feelsF = document.createElement('div'); 
+    let humidity = document.createElement('div'); 
+    feelsF.innerHTML = `Feels like: ${json.current.feelslike_f}℉`; 
+    humidity.innerHTML = `Humidity: ${json.current.humidity}%`; 
+    stats.append(feelsF, humidity); 
+}
